2f6d598a153c848ed096c9df3b968bc3b212344c
Vinicius Oliveira <vinicius.shk@gmail.com>
2023-02-16 19:30:05 -0300

    test: verify if count_ocurrences counts as intended, req 10

--- tests/counter/test_counter.py
+++ tests/counter/test_counter.py
@@ -1,5 +1,8 @@
-# from src.pre_built.counter import count_ocurrences
+from src.pre_built.counter import count_ocurrences
 
 
 def test_counter():
-    pass
+    js_counter = count_ocurrences('data/jobs.csv', r'/javascript/')
+    py_counter = count_ocurrences('data/jobs.csv', r'/python/')
+    assert js_counter > 0
+    assert py_counter > 0
