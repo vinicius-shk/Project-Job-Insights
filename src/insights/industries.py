3b97acb5e0a5edd89b20e69f9d32d1f924ea20f2
Vinicius Oliveira <vinicius.shk@gmail.com>
2023-02-16 17:06:39 -0300

    feat: implements filter_by_industry in industries.py, req 7

--- src/insights/industries.py
+++ src/insights/industries.py
@@ -40,4 +40,4 @@ def filter_by_industry(jobs: List[Dict], industry: str) -> List[Dict]:
     list
         List of jobs with provided industry
     """
-    raise NotImplementedError
+    return [row for row in jobs if row["industry"] == industry]
