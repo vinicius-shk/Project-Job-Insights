70f67a0e40b9985013a7f45b8a26d844ad181b2c
Vinicius Oliveira <vinicius.shk@gmail.com>
2023-02-17 14:34:59 -0300

    test:

--- tests/brazilian/test_brazilian_jobs.py
+++ tests/brazilian/test_brazilian_jobs.py
@@ -1,5 +1,11 @@
-# from src.pre_built.brazilian_jobs import read_brazilian_file
+from src.pre_built.brazilian_jobs import read_brazilian_file
 
 
 def test_brazilian_jobs():
-    pass
+    expected = read_brazilian_file('tests/mocks/brazilians_jobs.csv')
+    assert "title" in expected[0]
+    assert "titulo" not in expected[0]
+    assert "salary" in expected[0]
+    assert "salario" not in expected[0]
+    assert "type" in expected[0]
+    assert "tipo" not in expected[0]
--- tests/counter/test_counter.py
+++ tests/counter/test_counter.py
@@ -2,7 +2,7 @@ from src.pre_built.counter import count_ocurrences
 
 
 def test_counter():
-    js_counter = count_ocurrences('data/jobs.csv', r'/javascript/')
-    py_counter = count_ocurrences('data/jobs.csv', r'/python/')
-    assert js_counter > 0
-    assert py_counter > 0
+    js_counter = count_ocurrences('tests/mocks/jobs.csv', r'front')
+    py_counter = count_ocurrences('tests/mocks/jobs.csv', r'full')
+    assert js_counter == 1
+    assert py_counter == 3
